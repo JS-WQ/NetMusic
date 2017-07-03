6cda3fe83d7812a8c0958419b7849503003f5443
JSliuwenqiang <552410301@qq.com>
2017-07-03 20:51:15 +0800

    歌词

---
+++ index.js
@@ -0,0 +1,20 @@
+$(function(){
+    $.get('./song.json').then(function(response){
+        let items = response
+        items.forEach((i)=>{
+            let $li = $(`
+            <li>
+                <a href="./song.html?id=${i.id}">
+                    <h3>${i.name}</h3>
+                    <i></i>
+                    <p>演唱者-${i.singer}</p>
+                    <span></span>
+                </a>
+            </li>
+            `)
+            $('#newMusicList').append($li)
+        })
+        $('#musicloading').remove()
+    },function(){
+    })
+})
--- song.js
+++ song.js
@@ -1,4 +1,25 @@
 $(function(){
+    let id = parseInt(location.search.match(/\bid=([^&]*)/)[1],10)
+
+    $.get('./song.json').then(function(response){
+        let songs = response
+        let song = songs.filter(s=>s.id === id)[0]
+        let {url} = song
+        let audio = document.createElement('audio')
+        audio.src = url
+        audio.oncanplay=function(){
+            audio.play()
+            $('.song-container').addClass('playing')
+        }
+        $('.icon-pause').on('touchstart',function(){
+            audio.pause()
+            $('.song-container').removeClass('playing')
+        })
+        $('.icon-play').on('touchstart',function(){
+            audio.play()
+            $('.song-container').addClass('playing')
+        })
+    })
     $.get('/lyric.json').then(function(object){
         let {lyric} = object //等价于let lyric = object.lyric
         let array = lyric.split('\n') //把得到的数据分隔成不同的字符串
